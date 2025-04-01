64688786a20c62795ba458141fc73b26b71f1334
Vivaldo Chagas <67665267+Nosferatuvjr@users.noreply.github.com>
2025-04-01 15:19:51 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,7 @@
 <?php
-// Exemplo de vulnerabilidade: hardcoded credentials (Gitleaks detecta isso)
 $db_host = "localhost";
 $db_user = "root";
-$db_pass = "supersecretpassword"; // <- segredo no código
+$db_pass = "supersecretpassword";
 $db_name = "mydb";
 
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
