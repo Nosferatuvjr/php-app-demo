82f3943af96f7b79348aeb9e730d04efdd310cd1
Vivaldo Chagas <67665267+Nosferatuvjr@users.noreply.github.com>
2025-04-01 15:20:01 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,12 +1,11 @@
 <?php
 include('config.php');
 
-// SQL Injection clássica
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $user = $_POST['user'];
     $pass = $_POST['pass'];
     
-    $sql = "SELECT * FROM users WHERE username = '$user' AND password = '$pass'"; // <- SQLi
+    $sql = "SELECT * FROM users WHERE username = '$user' AND password = '$pass'";
     $result = mysqli_query($conn, $sql);
     
     if (mysqli_num_rows($result) > 0) {
